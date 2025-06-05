7fc73033e71d44f7483383d161e88c60285438ab
kirikoto <memberty.asis@gmail.com>
2025-06-05 18:22:40 +0800

    fixed scene inconsistency issue'

---
+++ main.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+func _ready():
+	# Load the scene to display
+	var new_scene = load("res://scenes/menu.tscn")
--- menu.gd
+++ menu.gd
@@ -1,7 +1,4 @@
 extends CanvasGroup
 
-
-
-
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://gameplay.tscn")
