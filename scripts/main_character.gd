35e46a23b45a0d16eebeb2a095985001f8f9fa73
kirikoto <memberty.asis@gmail.com>
2025-06-06 16:17:13 +0800

    added gun effect

--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -1,34 +1,94 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animated_sprite: AnimatedSprite2D = $movements
+@onready var smoke_effect: AnimatedSprite2D = $attackEffect
+@onready var crosshair_line: Line2D = $crosshairLine
+
 var last_direction: String = "down"
 
-func _physics_process(delta: float) -> void:
-	# Get the input direction
-	var direction = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
-	
-	# Set the velocity based on the input direction
-	velocity = direction * SPEED
+func _ready() -> void:
+	smoke_effect.hide()  # Hide by default
+	smoke_effect.connect("animation_finished", Callable(self, "_on_smoke_effect_animation_finished"))
 
-	# Move the character
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down").normalized()
+	velocity = direction * SPEED
 	move_and_slide()
 
-	# Update animation based on movement
 	if direction != Vector2.ZERO:
-		if abs(direction.x) > abs(direction.y):
-			if direction.x > 0:
-				animated_sprite.play("walk_right")
-				last_direction = "right"
-			else:
-				animated_sprite.play("walk_left")
-				last_direction = "left"
-		else:
-			if direction.y > 0:
-				animated_sprite.play("walk_down")
-				last_direction = "down"
-			else:
-				animated_sprite.play("walk_up")
-				last_direction = "up"
+		last_direction = get_direction_string(direction)
+		animated_sprite.play("walk_" + last_direction)
 	else:
 		animated_sprite.play("idle_" + last_direction)
+
+	# Update crosshair line with fixed length
+	var mouse_global_pos = get_global_mouse_position()
+	var to_mouse_local = to_local(mouse_global_pos)
+	
+	var max_length = 150.0  # max length of the crosshair line
+	
+	# Clamp vector length to max_length
+	if to_mouse_local.length() > max_length:
+		to_mouse_local = to_mouse_local.normalized() * max_length
+	
+	crosshair_line.clear_points()
+	crosshair_line.add_point(Vector2.ZERO)  # Player's center (local position)
+	crosshair_line.add_point(to_mouse_local)
+	
+	# Rotate the gun to point at the mouse
+	#var mouse_global_pos1 = get_global_mouse_position()
+	var gun_direction = (mouse_global_pos - global_position).normalized()
+	$Gun.rotation = gun_direction.angle()
+
+
+
+	# Optional: attack animation
+	if Input.is_action_just_pressed("white_attack"):
+		#white Attack
+		attack()
+		$Gun/gunAttack.play("white_attack")  # Play white gun firing animation
+		$Gun/whiteEffect.play("white_effect")  # Play white gun firing animation
+		
+	elif Input.is_action_just_pressed("black_attack"):
+		#black Attack
+		attack()
+		$Gun/gunAttack.play("black_attack") # Play black gun firing animation
+		$Gun/blackEffect.play("black_effect")  # Play white gun firing animation
+
+
+func get_direction_string(direction: Vector2) -> String:
+	if direction.x > 0.5:
+		if direction.y < -0.5:
+			return "upright"
+		elif direction.y > 0.5:
+			return "downright"
+		else:
+			return "right"
+	elif direction.x < -0.5:
+		if direction.y < -0.5:
+			return "upleft"
+		elif direction.y > 0.5:
+			return "downleft"
+		else:
+			return "left"
+	else:
+		if direction.y < 0:
+			return "up"
+		else:
+			return "down"
+
+func attack() -> void:
+	smoke_effect.frame = 0
+	smoke_effect.show()
+	smoke_effect.play("attackSmoke")
+	$Gun/gunAttack.stop()
+	$Gun/gunAttack.frame = 0
+	$Gun/blackEffect.stop()
+	$Gun/blackEffect.frame = 0
+	$Gun/whiteEffect.stop()
+	$Gun/whiteEffect.frame = 0
+	
+
+func _on_smoke_effect_animation_finished() -> void:
+	smoke_effect.hide()
